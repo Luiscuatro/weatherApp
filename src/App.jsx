1fe2402e04de67d7f7e6699cf5a205c2804e7456
Luiscuatro <Luisotonico@gmail.com>
2025-02-28 12:38:23 +0100

    fix: correct Route import and paths in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
-import { Routes } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+
+import ResponsiveAppBar from './layouts/Appbar'
+import Home from './pages/Home'
 import Weather from './pages/Weather'
+import Map from './pages/Map'
+import About from './pages/About'
 
 
 
 export default function App() {
   return (
-    <Routes>
-      <Rout path="/" element={<Home />} />
-      <Rout path="/weather" element={<Weather />} />
-      <Rout path="/map" element={<Map />} />
-      <Rout path="/about" element={<About />} />
-    </Routes>
+    <>
+      <ResponsiveAppBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/weather" element={<Weather />} />
+        <Route path="/map" element={<Map />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </>
   );
 }
 
